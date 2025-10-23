ab5ef7fc89fad6c04532967735693052ccebb67d
sreeks7675 <sreekarkashyap2210083@ssn.edu.in>
2025-10-24 01:04:35 +0530

    LoginPage.jsx changed

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+const API_BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : ''; 
 function LoginPage() {
   const handleLogin = () => {
-    window.location.href = '/api/auth/google';
+    const googleLoginUrl = `${API_BASE_URL}/api/auth/google`;
+    console.log("Redirecting to:", googleLoginUrl);
+    window.location.href = googleLoginUrl;
   };
   return (
     <div className="login-container">
